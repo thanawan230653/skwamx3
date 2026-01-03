d2dcc58d484ccf4b6f00fe8d920d441e0acb62f5
ธนาวัล กิจเจริญ <152389095+thanawan230653@users.noreply.github.com>
2026-01-03 11:08:25 +0700

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,23 @@
+@echo off
+cd bin
+
+
+ยินดีต้อนรับเด้
+
+
+title SYSTEM BREACH DETECTED
+mode con cols=120 lines=35
+cls
+
+
+echo #####################################################
+echo ###       ENTER FLASH ALL  SKWAMX3 OS12           ###
+echo ### Fastboot UNLOCK + FLASH ALL in Nony PPAN Not  ###
+echo #####################################################
+pause >nul
+start amlogic_tools_gui_v2.exe
+echo #####################################################
+echo # Not Suport FREE Verson 24.12.15.42        55555 ###
+echo #####################################################
+pause
+
